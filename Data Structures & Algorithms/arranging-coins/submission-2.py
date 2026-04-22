71d1aa31adb33cf477a0c4c0214c7d556e16c54c
yanjinghuang <yyyanjinghuang@gmail.com>
2026-04-21 22:14:55 -0700

    Add: arranging-coins - submission-2

---
+++ Data Structures & Algorithms/arranging-coins/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        l , r = 1, n 
+        res = 0
+        while l <= r:
+            mid =  (l + r) // 2 
+            coins = (mid * (mid + 1)) // 2
+            if coins > n:
+                r = mid - 1  
+            else:
+                l = mid + 1 
+                res = max(res, mid)
+        return res
+        
